1b972ff54996321e252cf7f7c519a23b02a710c9
Perez Ogayo <pogayo17@alustudent.com>
2021-01-04 02:17:55 +0200

    before writing the ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 
+#Project 2: Operationalizing Machine Learning
+##Project Overview
+##Architectural Diagram
+##Screenshots
+##ScreenCast: [YouTube link](https://youtu.be/Q0trirdPpRA)
--- enable_insights.py
+++
@@ -1,9 +0,0 @@
-from azureml.core import Workspace
-from azureml.core.webservice import Webservice
-
-ws = Workspace.from_config()
-
-
-aci_service = Webservice(ws, "bank-marketing")
-
-aci_service.update(enable_app_insights=True)
